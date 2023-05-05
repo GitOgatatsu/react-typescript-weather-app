829fa88ca9b31e1b82094450fb71c1e55ba490e9
herasake <ogawa.ogatatsu.tatsuya.jp@gmail.com>
2023-05-05 20:33:28 +0900

    2023/05/05 20:33

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,14 +1,22 @@
+// https://api.weatherapi.com/v1/current.json?key=3313f751bedb4a2996e105911230505&q=London&aqi=no
+
 import { useState } from "react";
 
 const Form = () => {
 
-	const [city, setCity] = useState("");
+	const [city, setCity] = useState<String>();
+
+	const getWeather = async (e: any) => {
+		await e.preventDefault();
+		await fetch("https://api.weatherapi.com/v1/current.json?key=3313f751bedb4a2996e105911230505&q=London&aqi=no")
+			.then(res => res.json())
+			.then(data => console.log(data))
+	};
 
 	return (
 		<form>
 			<input onChange={e => setCity(e.target.value)} type="text" name="city" placeholder="都市名"/>
-			{city}
-			<button>Get Weather</button>
+			<button onClick={getWeather}>Get Weather</button>
 		</form>
 	);
 
