4f24685589bdc970731468beb41f792b2b16b36c
herasake <ogawa.ogatatsu.tatsuya.jp@gmail.com>
2023-05-06 11:16:39 +0900

    2023/05/06 11:16

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
+import { useState } from "react";
+
 import Title from './components/Title';
 import Form from './components/Form';
+import Result from './components/Result';
 
 import './App.css';
 
 function App() {
+
+	const [city, setCity] = useState<String>();
+
+	const getWeather = async (e: any) => {
+		await e.preventDefault();
+		await fetch("https://api.weatherapi.com/v1/current.json?key=3313f751bedb4a2996e105911230505&q=London&aqi=no")
+			.then(res => res.json())
+			.then(data => console.log(data))
+	};
+
   return (
 		<div className="test">
 			<Title />
-			<Form />
+			<Form setCity={setCity} getWeather={getWeather} />
+			<Result />
 		</div>
   );
 }
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,22 +1,27 @@
 // https://api.weatherapi.com/v1/current.json?key=3313f751bedb4a2996e105911230505&q=London&aqi=no
 
-import { useState } from "react";
+//import { useState } from "react";
 
-const Form = () => {
+type FormPropsType = {
+	setCity: React.Dispatch<React.SetStateAction<String>>;
+	getWeather: (e: any) => Promise<void>;
+};
 
-	const [city, setCity] = useState<String>();
+const Form = (props: any) => {
 
-	const getWeather = async (e: any) => {
-		await e.preventDefault();
-		await fetch("https://api.weatherapi.com/v1/current.json?key=3313f751bedb4a2996e105911230505&q=London&aqi=no")
-			.then(res => res.json())
-			.then(data => console.log(data))
-	};
+//	const [city, setCity] = useState<String>();
+//
+//	const getWeather = async (e: any) => {
+//		await e.preventDefault();
+//		await fetch("https://api.weatherapi.com/v1/current.json?key=3313f751bedb4a2996e105911230505&q=London&aqi=no")
+//			.then(res => res.json())
+//			.then(data => console.log(data))
+//	};
 
 	return (
 		<form>
-			<input onChange={e => setCity(e.target.value)} type="text" name="city" placeholder="都市名"/>
-			<button onClick={getWeather}>Get Weather</button>
+			<input onChange={e => props.setCity(e.target.value)} type="text" name="city" placeholder="都市名"/>
+			<button onClick={props.getWeather}>Get Weather</button>
 		</form>
 	);
 
